259dca6176448363319e9a490ec05583b29f3848
gyj <gyj@gyjdeMacBook-Pro.local>
2016-12-30 10:11:42 +0800

    add file

---
+++ normalTest/trunc.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+ 
+int main()
+{
+	double x = 1.411, y = 1.500, z = 1.611;
+	cout << trunc(x) << endl;
+	cout << trunc(y) << endl;
+	cout << trunc(z) <<endl;
+ 
+	double a = -1.411, b = -1.500, c = -1.611;
+	cout << trunc(a) <<endl;
+	cout << trunc(b) <<endl;
+	cout << trunc(c) <<endl;
+	return 0;
+}
